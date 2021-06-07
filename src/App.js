ac72735848a7a0889d276a4b3db7dc414ee87fed
SiwooPak <siwoo1620@gmail.com>
2021-06-07 11:26:38 +0900

    modified: router,link, switch사용

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+
+// Home
+function Home() {
+  return <h1>Home</h1>;
+}
+
+function MyPage() {
+  return <h1>MyPage</h1>;
+}
+
+function DashBoard() {
+  return <h1>DashBoard</h1>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <nav>
+          <ul>
+            <li>
+              {/* Link 태그와 to속성을 이용하여 path 주소에 연결  */}
+              <Link to="/" >Home</Link>
+            </li>
+            <li>
+              <Link to="/about">MyPage</Link>
+            </li>
+            <li>
+              <Link to="/dashboard">DashBoard</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          {/* exact 주어진 경로와 정확히 일치해야만 그 컴포넌트를 보여줌. */}
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <MyPage />
+          </Route>
+          <Route path="/dashboard">
+            <DashBoard />
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
